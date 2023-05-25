ca765b73b8fb942e1700c01bc5748630fb3001f9
kchamithri <gangodaw-im18025@stu.kln.ac.lk>
2023-05-25 20:56:34 +0530

    dummy data

---
+++ data/ratings.js
@@ -0,0 +1,17 @@
+const rating = [
+  {
+    id: "1",
+    clientName: "Gimhan Ranasinghe",
+    description: "Best Physics teacher ever. Highly recommended.",
+    rating: 5,
+  },
+  {
+    id: "2",
+    clientName: "Naveen Silva",
+    description:
+      "I got a A for my physics paper thanks to Nishan sir. Recommended.",
+    rating: 4,
+  },
+];
+
+export default rating;
